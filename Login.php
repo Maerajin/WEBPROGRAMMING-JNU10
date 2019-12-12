2fbfef791480c49f910d8664839552a194fea949
wanseokim <koke7674@naver.com>
2019-12-12 19:25:37 +0900

    수정문
    로그인 창 수정

---
+++ Login.php
@@ -0,0 +1,30 @@
+<? 
+    session_start(); 
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<HTML>
+<HEAD>
+<meta http-equiv="Content-Type" Content="text/html"; charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css/jnulogin.css">
+<TITLE>JNU wiki Login</TITLE>
+</HEAD>
+<BODY>
+    <br/><br/><br/><br/>
+    <a href="Homepage.php"><h1>JNU wiki</h1></a>
+    <h2>Login</h2>
+    <hr/>
+    <form name="Join" method="post" action="Login.php">
+        <input name="id" type="text" placeholder="ID" style="width:500px; height:50px; border:3px solid blue; font-size:20px"/>
+        <br/>
+        <input name="pw" type="password" placeholder="Password" style="width:500px; height:50px; border:3px solid blue; font-size:20px"/>
+        <br/>
+        <input type="submit" value="Login" style="width:500px; height:30px; color:orangered; background-color:greenyellow;"/>
+        <center>
+            <div class="button">
+                <a href="Join.php" style="text-align:center; text-decoration:none; color:cornflowerblue; position:center;">Go to New join</a>
+            </div>
+        </center>
+
+    </form>
+</BODY>
+</HTML>
